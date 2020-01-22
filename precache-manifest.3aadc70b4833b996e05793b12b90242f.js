865618ba361c551b4bbcd0cf3357b2482e4f814c
OtacilioN <osmn@cesar.org.br>
2020-01-22 12:11:35 -0500

    Final version of project

---
+++ precache-manifest.3aadc70b4833b996e05793b12b90242f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0535323766b6d6c7f8c2d38927c5a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c49d1a6bcc60c19a085",
+    "url": "/static/css/main.5547585c.chunk.css"
+  },
+  {
+    "revision": "0b7bddb821a8d35b0f5e",
+    "url": "/static/js/2.58f58a2f.chunk.js"
+  },
+  {
+    "revision": "61d473203d676e321aa2d8c1ae85afa6",
+    "url": "/static/js/2.58f58a2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c49d1a6bcc60c19a085",
+    "url": "/static/js/main.8769819f.chunk.js"
+  },
+  {
+    "revision": "4ec23dac9d3ac8442d7d",
+    "url": "/static/js/runtime-main.ef66f3a6.js"
+  }
+]);
